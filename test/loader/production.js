b0c84ec8c610aebda93c0439f9e5dfe4be0961b6
Arjun Balla <arjun.balla@enterpaas.com>
2012-04-24 14:59:31 -0700

    Added loader to test stealing of all plugins

---
+++ test/loader/production.js
@@ -0,0 +1,4 @@
+steal("../../jqueryui.js").then(function($) {
+	$("body").append("<h2>Completed stealing of all jqueryui plugins!!!</h2>")
+	$("body").append("<h5>Note: You should not see any error on Firebug console</h5>")
+});
